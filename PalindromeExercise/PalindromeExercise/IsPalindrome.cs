660cb709c9553b4d0f07a3bfd7c023cc0a2cdeba
Damion Russell <domino.russell@gmail.com>
2022-07-25 13:15:37 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/IsPalindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class IsPalindrome
+    {
+        public bool IsAPalindrome(string str)
+        {
+            if(str == "" || str == null || str == " ")
+            {
+                return false;
+            }
+            var reversed = "";
+            for (int i = str.Length -1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            if(reversed == str)
+            {
+                return true;
+            }else return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("RaCeCaR", true)]
+        [InlineData("hello", false)]
+        [InlineData("", false)]
+        public void IsPalindrome(string word, bool expected)
         {
-
+            //arrange
+            var test = new IsPalindrome();
+            //act
+            var actual = test.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
